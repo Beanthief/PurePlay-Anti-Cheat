e1d88370864f3157fba5a419948e0b3900df41d3
Beanthief <wyattrosinski@gmail.com>
2024-10-12 14:41:43 -0400

    Added delays and reformatted keys to hashes

--- pythonapp/listener.py
+++ pythonapp/listener.py
@@ -2,18 +2,36 @@ import pynput # Only handles kbm
 import time
 import csv
 
+delayPrecision = 3
+batchDelay = 5
+
 keyList = []
 moveList = []
 clickList = []
+lastKeyTime = time.time()
+lastMoveTime = time.time()
+lastClickTime = time.time()
 
 def on_kpress(key):
-    keyList.append((1, key)) # translate keys to indeces
+    global lastKeyTime
+    delay = time.time() - lastKeyTime
+    lastKeyTime = time.time()
+    keyList.append((1, hash(key), round(delay, ndigits=delayPrecision)))
 def on_krelease(key):
-    keyList.append((0, key)) # translate keys to indeces
+    global lastKeyTime
+    delay = time.time() - lastKeyTime
+    lastKeyTime = time.time()
+    keyList.append((0, hash(key), round(delay, ndigits=delayPrecision)))
 def on_mmove(x, y):
-    moveList.append((x, y))
-def on_mclick(x, y, button, pressed): # consider mouse locations?
-    clickList.append((int(pressed), button)) # translate buttons to indeces
+    global lastMoveTime
+    delay = time.time() - lastMoveTime
+    lastMoveTime = time.time()
+    moveList.append((x, y, round(delay, ndigits=delayPrecision)))
+def on_mclick(x, y, button, pressed):
+    global lastClickTime
+    delay = time.time() - lastClickTime
+    lastClickTime = time.time()
+    clickList.append((int(pressed), x, y, hash(button), round(delay, ndigits=delayPrecision)))
     
 mouseListener = pynput.mouse.Listener(
     on_move=on_mmove,
@@ -27,7 +45,7 @@ keyboardListener.start()
 
 try:
     while True:
-        time.sleep(10)
+        time.sleep(batchDelay)
         with open("kb.csv", "w", newline='') as file:
             writer=csv.writer(file)
             for input in keyList:
@@ -43,7 +61,7 @@ try:
         keyList.clear()
         moveList.clear()
         clickList.clear()
-
+        
 except KeyboardInterrupt:
     print("Stopped")
     mouseListener.stop()
