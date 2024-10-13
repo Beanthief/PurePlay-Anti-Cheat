d80657c460d94749cd5dbc51d1b549a295a13382
Beanthief <wyattrosinski@gmail.com>
2024-10-12 20:53:41 -0400

    Began setup for config, added plotting and data splitting

--- pythonapp/main.py
+++ pythonapp/main.py
@@ -1,6 +1,50 @@
 import torch
 import pandas
+import models
 
-data = pandas.read_csv("inputs.csv", header=None)
-dataTensor = torch.tensor(data.values)
-print(dataTensor)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+keyData = pandas.read_csv("kb.csv", header=None)
+keyTensor = torch.tensor(keyData.values, dtype=torch.float64).to(device)
+print(keyTensor)
+
+moveData = pandas.read_csv("mmove.csv", header=None)
+moveTensor = torch.tensor(moveData.values, dtype=torch.float64).to(device)
+print(moveTensor)
+
+clickData = pandas.read_csv("mclick.csv", header=None)
+clickTensor = torch.tensor(clickData.values, dtype=torch.float64).to(device)
+print(clickTensor)
+
+keySplit = int(0.8 * len(keyTensor))
+moveSplit = int(0.8 * len(moveTensor))
+clickSplit = int(0.8 * len(clickTensor))
+
+keyTrain = keyTensor[:keySplit]
+keyTest = keyTensor[keySplit:]
+moveTrain = moveTensor[:moveSplit]
+moveTest = moveTensor[moveSplit:]
+clickTrain = clickTensor[:clickSplit]
+clickTest = clickTensor[clickSplit:]
+
+keyModel = models.RNN(inputCount=3, 
+                      nodeCount=3, 
+                      layerCount=3, 
+                      sequenceLength=2, 
+                      classCount=2).to(device)
+
+moveModel = models.RNN(inputCount=3, 
+                      nodeCount=3, 
+                      layerCount=3, 
+                      sequenceLength=2, 
+                      classCount=2).to(device)
+
+clickModel = models.RNN(inputCount=5, 
+                      nodeCount=3, 
+                      layerCount=3, 
+                      sequenceLength=2, 
+                      classCount=2).to(device)
+
+models.plotPredictions(keyModel, keyTrain, keyTest)
+models.plotPredictions(moveModel, moveTrain, moveTest)
+models.plotPredictions(clickModel, clickTrain, clickTest)
--- pythonapp/models.py
+++ pythonapp/models.py
@@ -1,10 +1,8 @@
 import torch
-import matplotlib # Add config to toggle
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import matplotlib.pyplot as plot # Add config to toggle
 
 # create separate models for keyboard, mouse move, and mouse clicks
-class RNN(torch.nn.Module()):
+class RNN(torch.nn.Module):
     def __init__(self, inputCount, nodeCount, layerCount, sequenceLength, classCount):
         super(RNN, self).__init__()
         self.inputCount = inputCount
@@ -19,10 +17,21 @@ class RNN(torch.nn.Module()):
         hiddenState0 = torch.zeros(self.layerCount, input.size(0), self.nodeCount).to(device)
 
         # Forward propagation
-        out, _ = self.network(x, hiddenState0)
-        out = out.reshape(out.shape[0], -1)
-        out = self.fcnetwork(out)
-        return out
-    
+        output, _ = self.network(input, hiddenState0)
+        output = output.reshape(output.shape[0], -1)
+        output = self.fcnetwork(output)
+        return output
+
+def plotPredictions(model, trainSplit, testSplit, predictions=None):
+    plot.figure(figsize=(10, 10))
+    plot.scatter(trainSplit, trainSplit, c="b", s=6, label="Training Data") # c = color
+    plot.scatter(testSplit, testSplit, c="g", s=6, label="Testing Data") # s = dot thickness
+    # To make an inference using your model, use the following
+    with torch.inference_mode():
+        predictions = model(testSplit)
+    plot.scatter(testSplit, predictions, c="r", s=6, label="Predictions")
+    plot.legend(prop={"size":14})
+    plot.show()
+
 def train(model, learnRate, dataTensor):
-    print() # populate with training loop with matplotlib output config
+    print()
