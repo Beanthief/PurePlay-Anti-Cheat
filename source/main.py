7016eb4c87af27dfe605e99b9052a87d7fdfe686
Beanthief <wyattrosinski@gmail.com>
2025-01-24 22:41:51 -0500

    Removed asynchronous listener, replaced with polling, changed model and data divisions

--- source/main.py
+++ source/main.py
@@ -1,35 +1,33 @@
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
-import tensorflow as tf
 import configparser
+import tensorflow
+import threading
 import keyboard
-import listener
 import pandas
+import XInput
+import mouse
 import keras
 import time
+import csv
 import os
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-programMode = int(config['General']['programMode'])                # 0 = Data Collection, 1 = Model Training, 2 = Live Analysis
-saveInterval = int(config['Collection']['saveInterval'])                 
-captureKeyboard = int(config['Collection']['captureKeyboard'])
-captureMouse = int(config['Collection']['captureMouse'])
-captureController = int(config['Collection']['captureController']) # DO NOT ENABLE AT SAME TIME AS KB OR MOUSE
-downSampleFactor = int(config['Collection']['downSampleFactor'])   # Downsampling factor for mouse events
-dataLabel = config['Collection']['dataLabel']                      # control, cheat
-killKey = config['Collection']['killKey']
-batchSize = int(config['Training']['batchSize'])
-learningRate = float(config['Training']['learningRate'])
-trainingEpochs = int(config['Training']['trainingEpochs'])
-pollInterval = float(config['Analysis']['pollInterval'])
-displayGraph = int(config['Analysis']['displayGraph'])
+programMode =       int(config['General']['programMode'])       # 0 = Data Collection, 1 = Model Training, 2 = Live Analysis
+captureKBM =        int(config['General']['captureKBM'])
+captureController = int(config['General']['captureController'])
+pollInterval =    float(config['General']['pollInterval'])      # in milliseconds
+killKey =               config['General']['killKey']
+dataLabel =             config['Collection']['dataLabel']       # control, cheat, etc.
+saveInterval =      int(config['Collection']['saveInterval'])
+batchSize =         int(config['Training']['batchSize'])
+learningRate =    float(config['Training']['learningRate'])
+trainingEpochs =    int(config['Training']['trainingEpochs'])
+displayGraph =      int(config['Reporting']['displayGraph'])
 
-# device = 'cuda' if len(keras.backend.tensorflow_backend._get_available_gpus()) > 0 else 'cpu'
-encoder = LabelEncoder()
+print('Using device:', tensorflow.config.list_physical_devices('GPU'))
 scaler = StandardScaler()
-inputListener = listener.InputListener(captureKeyboard, captureMouse, captureController, downSampleFactor, (0))
 
 def BinaryLSTM(inputShape):
     model = keras.Sequential()
@@ -41,6 +39,61 @@ def BinaryLSTM(inputShape):
     model.summary()
     return model
 
+keys = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    '+', '-', '*', '/', '.', ',', '<', '>', '?', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '"', "'", '`', '~',
+    'enter', 'esc', 'backspace', 'tab', 'space',
+    'caps lock', 'num lock', 'scroll lock',
+    'home', 'end', 'page up', 'page down', 'insert', 'delete',
+    'left', 'right', 'up', 'down',
+    'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12',
+    'print screen', 'pause', 'break', 'windows', 'menu'
+    ] + list(keyboard.all_modifiers)
+kbmFeatures = len(keys) + 5 # 5 mouse features
+kbmData = []
+controllerData = []
+
+def poll_inputs():
+    kbmList = []
+    controllerList = []
+    if captureKBM:
+        kbmList = [1 if keyboard.is_pressed(key) else 0 for key in keys]
+        if mouse.is_pressed(button='left'):
+            kbmList.append(1)
+        else:
+            kbmList.append(0)
+        if mouse.is_pressed(button='right'):
+            kbmList.append(1)
+        else:
+            kbmList.append(0)
+        if mouse.is_pressed(button='middle'):
+            kbmList.append(1)
+        else:
+            kbmList.append(0)
+        kbmList.extend(mouse.get_position())
+    if captureController:
+        if XInput.get_connected():
+            controllerList = [int(value) for value in XInput.get_button_values(XInput.get_state(0)).values()]
+            controllerList.extend(XInput.get_thumb_values(XInput.get_state(0)))
+            controllerList.extend(XInput.get_trigger_values(XInput.get_state(0)))
+        else:
+            print('Controller not connected')
+    return kbmList, controllerList
+
+def save_data():
+    while True:
+        time.sleep(saveInterval)
+        if kbmData:
+            with open(f'data/kbm{dataLabel}Data.csv', 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(kbmData)
+        if controllerData:
+            with open(f'data/controller{dataLabel}Data.csv', 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(controllerData)
+
 def update_graph(confidenceValues):
     plt.clf()
     plt.plot(confidenceValues)
@@ -50,83 +103,66 @@ def update_graph(confidenceValues):
     plt.pause(0.01)
 
 match programMode:
-
     ########## Data Collection ##########
     case 0:
-        inputListener.start()
+        os.makedirs('data', exist_ok=True)
+        threading.Thread(target=save_data, daemon=True).start()
         while True:
-            time.sleep(saveInterval)
+            time.sleep(pollInterval/1000)
             if keyboard.is_pressed(killKey):
-                inputListener.stop()
                 os._exit(0)
-            inputListener.save_to_files(dataLabel)
-
+            kbmRead, controllerRead = poll_inputs()
+            if kbmRead:
+                kbmData.append(kbmRead)
+            if controllerRead:
+                controllerData.append(controllerRead)
+    
     ########## Model Training ##########
     case 1:
-        buttonLSTM = BinaryLSTM(inputShape=(batchSize, 4))
-        if captureMouse:
-            moveLSTM = BinaryLSTM(inputShape=(batchSize, 2))
+        if captureKBM:
+            kbmLSTM = BinaryLSTM(inputShape=(batchSize, kbmFeatures))
         if captureController:
-            stickLSTM = BinaryLSTM(inputShape=(batchSize, 3))
-            triggerLSTM = BinaryLSTM(inputShape=(batchSize, 3))
+            controllerLSTM = BinaryLSTM(inputShape=(batchSize, 16)) # 16 controller features
 
-        buttonX = []
-        buttonY = []
-        moveX = []
-        moveY = []
-        stickX = []
-        stickY = []
-        triggerX = []
-        triggerY = []
+        kbmX = []
+        kbmY = []
+        controllerX = []
+        controllerY = []
 
         for fileName in os.listdir("data"):
             filePath = os.path.join("data", fileName)
             if os.path.isfile(filePath) and fileName.endswith('.csv'):
                 dataFrame = pandas.read_csv(filePath)
-                dataArray = dataFrame.to_numpy() # Need to encode strings and normalize
-                inputData = dataArray[:, 1:]
-                knownClasses = dataArray[:, 0]
-                
-                print(inputData)
-                if 'button' in fileName:
-                    buttonX.append(inputData)
-                    buttonY.append(knownClasses)
-                elif 'move' in fileName:
-                    moveX.append(inputData)
-                    moveY.append(knownClasses)
-                elif 'stick' in fileName:
-                    stickX.append(inputData)
-                    stickY.append(knownClasses)
-                elif 'trigger' in fileName:
-                    triggerX.append(inputData)
-                    triggerY.append(knownClasses)
+                dataArray = dataFrame.to_numpy()
+                # inputData = 
+                # knownClasses = 
+                # if fileName.startswith('kbm'):
+                #     kbmX.append(inputData)
+                #     kbmY.append(knownClasses)
+                # elif fileName.startswith('controller'):
+                #     controllerX.append(inputData)
+                #     controllerY.append(knownClasses)
 
         os.makedirs('models', exist_ok=True)
 
-        if buttonX:
-            buttonLSTM.fit(buttonX, buttonY, epochs=trainingEpochs)
-            buttonLSTM.save('models/button.keras')
-        if moveX:
-            moveLSTM.fit(moveX, moveY, epochs=trainingEpochs)
-            moveLSTM.save('models/move.keras')
-        if stickX:
-            stickLSTM.fit(stickX, stickY, epochs=trainingEpochs)
-            stickLSTM.save('models/stick.keras')
-        if triggerX:
-            triggerLSTM.fit(triggerX, triggerY, epochs=trainingEpochs)
-            triggerLSTM.save('models/trigger.keras')
+        if kbmX:
+            kbmLSTM.fit(kbmX, kbmY, epochs=trainingEpochs)
+            kbmLSTM.save('models/kbm.keras')
+        if controllerX:
+            controllerLSTM.fit(controllerX, controllerY, epochs=trainingEpochs)
+            controllerLSTM.save('models/controller.keras')
 
     ########## Live Analysis ##########
     case 2: 
-        inputListener.start()
-
         confidence_values = []
-        buttonLSTM = keras.saving.load_model('models/button.keras')
-        if captureMouse:
-            moveLSTM = keras.saving.load_model('models/move.keras')
-        if captureController:
-            stickLSTM = keras.saving.load_model('models/stick.keras')
-            triggerLSTM = keras.saving.load_model('models/trigger.keras')
+
+        if captureKBM:
+            kbmLSTM = keras.saving.load_model('models/kbm.keras')
+        elif captureController:
+            controllerLSTM = keras.saving.load_model('models/controller.keras')
+        else:
+            print('No models loaded')
+            os._exit(0)
 
         plt.ioff()
         plt.figure()
@@ -135,36 +171,19 @@ match programMode:
             plt.show()
 
         while True: # Or while game is running?
-            time.sleep(pollInterval)
+            time.sleep(pollInterval/1000)
             if keyboard.is_pressed(killKey):
-                inputListener.stop()
                 plt.savefig('confidence_graph.png')
                 os._exit(0)
+            kbmData, controllerData = poll_inputs()
             confidence = 1
-            if captureKeyboard:
-                input_data = scaler.fit_transform(inputListener.buttonData)
-                inputListener.buttonData = []
-                output = buttonLSTM.predict(input_data)
-                confidence *= tf.nn.softmax(output)[0][1]
-                    
-            if captureMouse:
-                input_data = scaler.fit_transform(inputListener.moveData)
-                inputListener.moveData = []
-                output = moveLSTM.predict(input_data)
-                confidence *= tf.nn.softmax(output)[0][1]
-                    
+            if captureKBM:
+                input_data = scaler.fit_transform(kbmData)
+                output = kbmLSTM.predict(input_data) # How to predict?
+                confidence *= output[0][1]
             if captureController:
-                input_data = scaler.fit_transform(inputListener.stickData)
-                inputListener.stickData = []
-                output = stickLSTM.predict(input_data)
-                confidence *= tf.nn.softmax(output)[0][1]
-                    
-                input_data = scaler.fit_transform(inputListener.triggerData)
-                inputListener.triggerData = []
-                output = triggerLSTM.predict(input_data)
-                confidence *= tf.nn.softmax(output)[0][1]
-                    
+                input_data = scaler.fit_transform(controllerData)
+                output = controllerLSTM.predict(input_data) # How to predict?
+                confidence *= output[0][1]
             confidence_values.append(confidence)
             update_graph(confidence_values)
-    
-        inputListener.stop()
